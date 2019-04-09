6ce86e624790898ba36d3b181711efcec50597e2
Keila <keiladaianefernandes24@gmail.com>
2019-04-09 19:36:27 -0300

    iniciando projeto

---
+++ src/main/java/com/eventoapp/eventoapp/IndexController.java
@@ -0,0 +1,12 @@
+package com.eventoapp.eventoapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class IndexController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Index";
+	}
+	
+}
